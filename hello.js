b21828d46034ed78f14ae7c8a0573fd5254038d0
Raphael <81021823+Polar-69@users.noreply.github.com>
2021-08-23 07:57:55 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
-var Hello = "Hey Whatcha Doing Looking Here";
+var Hey = "Hey Nothing To See Here || Go Back To README LOL";
+
+let John = 1;
+
+let Adam = 2;
+
+let Bill = 3;
+
+let Jill = 4;
+
+let Liam = 5;
+
+let Jack = 6;
+
+let Noah = 7;
+
+let Luke = 8;
+
+let Levi = 9;
+
+var Potato = Number('1234');
